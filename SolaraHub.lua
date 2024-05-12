36b56abd0202e1402fa58ae9522ed6cb7f861511
tippy2235 <123338167+tippy2235@users.noreply.github.com>
2024-05-12 19:55:05 -0400

    Update SolaraHub.lua

--- SolaraHub.lua
+++ SolaraHub.lua
@@ -1,178 +1,204 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Solara Hub", "DarkTheme")
+local ezlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/debug420/Ez-Hub/master/Modules/EzLib.lua"))();
+local mainGUI = ezlib.create("My Script");
 
 
 ---Tabs
-local Dahoodtab = Window:NewTab("Da-Hood")
-local Arsenaltab = Window:NewTab("Arsenal")
-local BladeBall = Window:NewTab("BladeBall")
-local PhantomForces = Window:NewTab("Phantom Forces")
-local DustyTrip = Window:NewTab("Dusty Trip")
-local MURDERMYSTER2 = Window:NewTab("MURDER-MYSTER-2")
-local BloxFruits = Window:NewTab("BloxFruits")   
-local CounterBlox = Window:NewTab("CounterBlox") 
-local TowerOfHell = Window:NewTab("Tower Of Hell")   
-local BuildABoat = Window:NewTab("Build A Boat")
-local UTG = Window:NewTab("Untitled-Tag-Game")  
-local FleeTheFacility = Window:NewTab("Flee The Facility")
-local AntiAfk = Window:NewTab("Anti-Afk")    
-local PetSim99 = Window:NewTab("PetSim99")  
-local DrivingEmpire = Window:NewTab("Driving Empire")    
-local SlapBattles = Window:NewTab("Slap-Battles")
-local NavalWarfare = Window:NewTab("Naval Warfare")
-
+local Dahoodtab = mainGUI.newTab("Da-Hood");
+local Arsenaltab = mainGUI.newTab("Arsenal");
+local BladeBall = mainGUI.newTab("BladeBall");
+local PhantomForces = mainGUI.newTab("Phantom Forces");
+local DustyTrip = mainGUI.newTab("Dusty Trip");
+local MURDERMYSTER2 = mainGUI.newTab("MURDER-MYSTER-2");
+local BloxFruits = mainGUI.newTab("BloxFruits");
+local CounterBlox = mainGUI.newTab("CounterBlox");
+local TowerOfHell = mainGUI.newTab("Tower Of Hell");
+local BuildABoat = mainGUI.newTab("Build A Boat");
+local UTG = mainGUI.newTab("Untitled-Tag-Game");
+local FleeTheFacility = mainGUI.newTab("Flee The Facility");
+local AntiAfk = mainGUI.newTab("Anti-Afk");
+local PetSim99 = mainGUI.newTab("PetSim99");
+local DrivingEmpire = mainGUI.newTab("Driving Empire");
+local SlapBattles = mainGUI.newTab("Slap-Battles");
+local NavalWarfare = mainGUI.newTab("Naval Warfare");
+local WorkAtPizzaPlace = mainGUI.newTab("Work At Pizza Place");
+local DDayScript = mainGUI.newTab("D-DayScript");
 
 
 
 ---Tab Sections
-local DahoodSection = Dahoodtab:NewSection("Da-Hood Scripts")
-local ArsenalSection = Arsenaltab:NewSection("Arsenal Scripts")
-local BladeBallSection = BladeBall:NewSection("BladeBall Scripts")
-local PhantomForcesSection = PhantomForces:NewSection("PhantomForces Scripts")
-local DustyTripSection = DustyTrip:NewSection("DustyTrip Scripts")
-local MURDERMYSTER2Section = MURDERMYSTER2:NewSection("murder-mystery-2 Scripts")
-local BloxFruitsSection = BloxFruits:NewSection("BloxFruits Scripts")
-local CounterBloxSection = CounterBlox:NewSection("CounterBlox Scripts")
-local TowerOfHellSection = TowerOfHell:NewSection("Tower Of Hell Scripts")
-local BuildABoatSection = BuildABoat:NewSection("Build A Boat Scripts")  
-local UTGSection = UTG:NewSection("Untitled-Tag-Game Scripts") 
-local FleeSection = FleeTheFacility:NewSection("Flee The Facility Scripts")  
-local AntiAfkSection = AntiAfk:NewSection("Anti-Afk Scripts")
-local PetSim99Section = PetSim99:NewSection("PetSim99 Scripts")  
-local DrivingEmpireSection = DrivingEmpire:NewSection("DrivingEmpire Scripts") 
-local SlapBattlesSection = SlapBattles:NewSection("SlapBattles Scripts")
-local NavalWarfareSection = NavalWarfare:NewSection("NavalWarfare Scripts")
-
-
-
----Buttons
-
----Dahood
-DahoodSection:NewButton("Unownaz--Epic", "Da-Hood", function()
+Dahoodtab.newTitle("Da-Hood Scripts");
+Dahoodtab.newDiv();
+Dahoodtab.newButton("Unownaz--Epic", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tippy2235/Unownaz--Epic/main/Unownaz--Epic.lua"))()
 end)
 
-DahoodSection:NewButton("Azure Modded", "Da-Hood", function()
-loadstring(game:HttpGet("https://pastebin.com/raw/neMabYbU"))()
+Dahoodtab.newButton("Azure Modded", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/neMabYbU"))()
 end)
 
-DahoodSection:NewButton("Capslock", "Da-Hood", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/whoiscapslock/capslock/main/main", true))()
+Dahoodtab.newButton("Capslock", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/whoiscapslock/capslock/main/main", true))()
 end)
 
+---------------------------------------------------------Arsenal
 
----Arsenal
-ArsenalSection:NewButton("Inf Yeild", "Arsenal/All", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tippy2235/Unownaz--Epic/main/Arsenal.lua", true))()
+Arsenaltab.newTitle("Arsenal Scripts");
+Arsenaltab.newDiv();
+Arsenaltab.newButton("Inf Yeild", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tippy2235/Unownaz--Epic/main/Arsenal.lua", true))()
 end)
 
-ArsenalSection:NewButton("Leg Hub", "Arsenal", function()
+Arsenaltab.newButton("Leg Hub", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/cris123452/my/main/cas",true))()
 end)
 
+----------------------------------------------------------BladeBall
 
----BladeBall
-BladeBallSection:NewButton("Auto Parry", "BladeBall", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cris123452/my/main/cas",true))()
+BladeBall.newTitle("BladeBall Scripts");
+BladeBall.newDiv();
+BladeBall.newButton("Auto Parry", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/autoparry.lua"))()
 end)
 
 
----PhantomForces
-
-
-
----Dusty Trip
-DustyTripSection:NewButton("Dusty Trip", "Dusty Trip", function()
+---------------------------------------------------------Dusty Trip
+DustyTrip.newTitle("Dusty Trip Scripts");
+DustyTrip.newDiv();
+DustyTrip.newButton("Dusty Trip", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/artemy133563/Utilities/main/ADustyTrip",true))()
 end)
 
+---------------------------------------------------------MM2
 
----murder-mystery-2   
-MURDERMYSTER2Section:NewButton("murder-mystery-2", "murder-mystery-2", function()
+MURDERMYSTER2.newTitle("murder-mystery-2 Scripts");
+MURDERMYSTER2.newDiv();
+MURDERMYSTER2.newButton("murder-mystery-2", function()
     loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
 end)
 
+---------------------------------------------------------BloxFruits
 
----Blox Fruits
-BloxFruitsSection:NewButton("BloxFruits", "BloxFruits", function()
+BloxFruits.newTitle("BloxFruits Scripts");
+BloxFruits.newDiv();   
+BloxFruits.newButton("BloxFruits", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Yatsuraa/Yuri/main/Winterhub_V2.lua"))()
 end)
 
+---------------------------------------------------------CounterBlox
 
----CounterBlox
-CounterBloxSection:NewButton("CounterBlox", "CounterBlox", function()
+CounterBlox.newTitle("CounterBlox Scripts");
+CounterBlox.newDiv();   
+CounterBlox.newButton("CounterBlox", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/fliskScript/jaran.vip/main/free.lua"))()
 end)
 
+---------------------------------------------------------TowerOfHell
 
----TowerOfHell 
-TowerOfHellSection:NewButton("Tower Of Hell", "Tower Of Hell", function()
+TowerOfHell.newTitle("TowerOfHell Scripts");
+TowerOfHell.newDiv();   
+TowerOfHell.newButton("TowerOfHell", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/sc0ttferren/robloxscripts/main/toh/sc0tt.lua'))()
 end)
 
+---------------------------------------------------------Build A Boat
 
----Build A Boat
-BuildABoatSection:NewButton("BABFT", "BABFT", function()
+BuildABoat.newTitle("BuildABoat Scripts");
+BuildABoat.newDiv();   
+BuildABoat.newButton("BuildABoat", function()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/babft"))()
 end)
 
+---------------------------------------------------------UTG
 
----Utg 
-UTGSection:NewButton("Ranxware", "UntitledTagGame", function()
+UTG.newTitle("Untitled-Tag-Game Scripts");
+UTG.newDiv();   
+UTG.newButton("Untitled-Tag-Game", function()
     loadstring(gameHttpGet(httpsraw.githubusercontent.comZer0idsQwertymainUntitledTagNewScript.lua))()
 end)
 
+---------------------------------------------------------FleeTheFacility  
 
----FleeTheFacility  
-FleeSection:NewButton("EVA", "FleeTheFacility", function()
+FleeTheFacility.newTitle("Untitled-Tag-Game Scripts");
+FleeTheFacility.newDiv();   
+FleeTheFacility.newButton("EVA", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/K9ntra/EVA/main/abc.lua'))()
 end)
 
 
----Anti-Afk 
-AntiAfkSection:NewButton("Anti-Afk ", "All Games", function()
-    wait(0.5)local ba=Instance.new("ScreenGui")
-local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
-local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
-ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
-ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
-ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
-ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Solara Hub Anti Afk"ca.TextColor3=Color3.new(0,1,1)
-ca.TextSize=22;da.Parent=ca
-da.BackgroundColor3=Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
-da.Size=UDim2.new(0,370,0,107)_b.Parent=da
-_b.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
-_b.Size=UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text="Solara Hub .gg/FVZWU3E6Kx"
-_b.TextColor3=Color3.new(0,1,1)_b.TextSize=20;ab.Parent=da
-ab.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377,0)
-ab.Size=UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Protected"
-ab.TextColor3=Color3.new(0,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
-game:service'Players'.LocalPlayer.Idled:connect(function()
-bb:CaptureController()bb:ClickButton2(Vector2.new())
-ab.Text="Solara Hub prevented robloxs kick!"wait(2)ab.Text="Status : Protected"end)
+------------------------------------------------------------Anti-Afk 
+
+AntiAfk.newTitle("AntiAfk | All Games");
+AntiAfk.newDiv();   
+AntiAfk.newButton("AntiAfk", function()
+    AntiAfkSection:NewButton("Anti-Afk ", "All Games", function()
+        wait(0.5)local ba=Instance.new("ScreenGui")
+    local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
+    local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
+    ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
+    ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
+    ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
+    ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Solara Hub Anti Afk"ca.TextColor3=Color3.new(0,1,1)
+    ca.TextSize=22;da.Parent=ca
+    da.BackgroundColor3=Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
+    da.Size=UDim2.new(0,370,0,107)_b.Parent=da
+    _b.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
+    _b.Size=UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text="Solara Hub .gg/FVZWU3E6Kx"
+    _b.TextColor3=Color3.new(0,1,1)_b.TextSize=20;ab.Parent=da
+    ab.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377,0)
+    ab.Size=UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Protected"
+    ab.TextColor3=Color3.new(0,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
+    game:service'Players'.LocalPlayer.Idled:connect(function()
+    bb:CaptureController()bb:ClickButton2(Vector2.new())
+    ab.Text="Solara Hub prevented robloxs kick!"wait(2)ab.Text="Status : Protected"end)
+    end)
 end)
 
+------------------------------------------------------------PetSim99
 
----PetSim99
-PetSim99Section:NewButton("Pet Sim 99", "Pet sim 99", function()
+PetSim99.newTitle("Pet Sim 99 Scripts");
+PetSim99.newDiv();   
+PetSim99.newButton("Pet Sim 99", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/tippy2235/Unownaz--Epic/main/Petsim99.lua'))()
 end)
 
+------------------------------------------------------------Driving Empire
 
----Driving Empire
-DrivingEmpireSection:NewButton("DrivingEmpire AutoFarm", "DrivingEmpire", function()
+DrivingEmpire.newTitle("DrivingEmpire Scripts");
+DrivingEmpire.newDiv();   
+DrivingEmpire.newButton("DrivingEmpire AutoFarm", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Marco8642/science/main/drivingempire", true))()
 end)
 
+------------------------------------------------------------Slap-Battles
 
----Slap-Battles
-SlapBattlesSection:NewButton("Slap Battles - Gaster Hub", "Slap Battles", function()
+SlapBattles.newTitle("SlapBattles Scripts");
+SlapBattles.newDiv();   
+SlapBattles.newButton("Slap Battles - Gaster Hub", function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Dusty1234567890/TestMain/main/SLAPBATTLESGUI5")))()
 end)
 
+------------------------------------------------------------Naval Warfare
 
----Naval Warfare
-NavalWarfareSection:NewButton("NavalWarfare", "NavalWarfare", function()
+NavalWarfare.newTitle("NavalWarfare Scripts");
+NavalWarfare.newDiv();   
+NavalWarfare.newButton("NavalWarfare", function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/tippy2235/Unownaz--Epic/main/NavalWarfare.lua")))()
 end)
 
+
+------------------------------------------------------------WorkAtPizzaPlace
+
+WorkAtPizzaPlace.newTitle("Work At Pizza Place Scripts");
+WorkAtPizzaPlace.newDiv();   
+WorkAtPizzaPlace.newButton("WAPP", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/7Q5sN2wY",true))()
+end)
+
+
+------------------------------------------------------------DDayScript
+
+DDayScript.newTitle("D Day Script Scripts");
+DDayScript.newDiv();   
+DDayScript.newButton("D Day Script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GwnStefano/NexusHub/main/Main", true))()
+end)
